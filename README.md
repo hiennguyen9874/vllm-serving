c819d86c845cf2ea12c51e2f7b4329554cd1405c
Xuan Hien <hiennguyen9874@gmail.com>
2025-05-05 09:21:20 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vLLM Serving
+
+
+## Qwen2.5-VL-72B-Instruct-AWQ
+
+```bash
+
+
+```
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vllm-serving!")
+
+
+if __name__ == "__main__":
+    main()
